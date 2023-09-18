467ecfe266b9196e4754ffb29ed0741ee566cbbd
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-09-18 10:26:31 +0100

    Updated Questions 3,4,5

--- solc.js
+++ solc.js
@@ -26,3 +26,29 @@ const calcAv = function (data) {
 };
 
 calcAv(data1);
+
+// Question 3
+/* -- What do you understand about JavaScript?
+JavaScript is a popular web-scripting language and is used for client-side and sever side development */
+
+// Question 4
+/* -- What's the difference between javaScript and Java?
+JavaScript is Object-oriented scripting language && JavaScript applications are meant to run inside a web browser && Javascript does not need compilation before running the application code.
+Java is an Object-oriented programming language && Java application are generally made for use in Operating Systems and Virtual Machines && Java Source code needs a compiler before it can be ready to run in realtime*/
+
+// Question 5
+/* -- What are the various data types in JavaScript ?
+ Data type --- Description 
+ Boolean   ==>  For true and false values
+ Null      ==>  For empty or unknown values
+ Undefined ==>  For variables that are only declared and not defined or initialized 
+ Number    ==>  For integer and floating-point values
+ String    ==>  For characters and alphanumeric values
+ Object    ==>  For collections or complex values
+ Symbols   ==>  For unique identifiers or objects 
+ */
+
+// Question 6
+const data = function () {
+  console.log("3,4,5");
+};
